769d341bdb4ed0e9da48392e6713552960caf1db
jggranito <jggranito@gmail.com>
2024-05-20 16:51:07 -0300

    adicionada correção de filmes duplicados

--- src/server.ts
+++ src/server.ts
@@ -25,6 +25,14 @@ app.post("/movies", async (req, res) => {
     const { title, genre_id, language_id, oscar_count, release_date } = req.body;
 
     try {
+        const movieWithSameTitle = await prisma.movie.findFirst({
+            where: { title: {equals: title, mode: "insensitive"} }
+        });
+
+        if(movieWithSameTitle) {
+            return res.status(409).send({ message: "Filme já cadastrado" });
+        }
+
         await prisma.movie.create({
             data: {
                 title,
@@ -34,8 +42,8 @@ app.post("/movies", async (req, res) => {
                 release_date: new Date(release_date)
             }
         });
-    }catch(error){
-        return res.status(500).send({message: "falha ao cadastrar um filme"});
+    } catch (error) {
+        return res.status(500).send({ message: "falha ao cadastrar um filme" });
     }
 
     res.status(201).send();
